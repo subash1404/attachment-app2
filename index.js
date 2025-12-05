2fa8c3ccae397ccfe256138f48bd8131299a333d
prajwal <prajwal.pandi@superops.com>
2025-12-05 14:26:53 +0530

    headers 2

--- index.js
+++ index.js
@@ -163,7 +163,7 @@ app.get('/webview', (req, res) => {
       <body>
         <div class="container">
           <h2>Attach files</h2>
-          <p style="color:#777; margin-top:-5px;">Select files to upload</p>
+          <p style="color:#777; margin-top:-5px;">Select file to upload</p>
 
           <div class="upload-box" onclick="document.getElementById('fileInput').click()">
             <svg xmlns="http://www.w3.org/2000/svg" width="74" height="69" viewBox="0 0 74 69" fill="none">
