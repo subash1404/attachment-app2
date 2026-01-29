01ff5ca19cb3dddbbd187e6dab56585a4db88bd1
prajwal <prajwal.pandi@superops.com>
2026-01-29 14:29:43 +0530

    adnjdn

--- index.js
+++ index.js
@@ -277,10 +277,7 @@ app.get('/webview', (req, res) => {
 
               const ctx = await getTeamsContextAsync();
               console.log("ctx:", ctx);
-              formData.append("teamsTenantId", ctx.tid);
-              formData.append("teamsUserObjectId", ctx.userObjectId);
-              formData.append("teamsConversationType", ctx.conversationType);
-              formData.append("teamsHostClientType", ctx.hostClientType);
+              formData.append("teamsContext", JSON.stringify(ctx));
 
               // Attach files AFTER token
               selectedFiles.forEach(f => formData.append("attachments", f));
